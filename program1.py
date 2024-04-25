68fd07dd91999ea49fd61c922b6176835933807b
Shirshchauhan <106151600+Shirshchauhan@users.noreply.github.com>
2024-04-25 14:00:16 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,21 +1,4 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-    
-      stack = []
-      mapping = {')': '(', ']': '[', '}': '{'}
-      for char in s:
-            if char in mapping.values():
-                stack.append(char)
-            elif char in mapping.keys():
-                if not stack or mapping[char] != stack.pop():
-                    return False
-            else:
-                return False
-        return not stack
-        :type s: str
-        :rtype: bool
-        """
+
         pass
     
 
